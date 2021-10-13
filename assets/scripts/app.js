b6fdee2aff47ca42800ceb192b7f18809265821d
RobertBognar <robertbognar2@gmail.com>
2021-10-13 13:15:55 +0200

    preparing API

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+//Importing HTML attributes
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+const body = document.querySelector('body');
+
+
+//API Keys & Values
+
+
+
